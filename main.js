e48283d4cbe2ed2610c43519448c6cbc631ef5dd
Josan866 <joshua.millner@online.de>
2024-08-29 23:10:42 +0200

    Update main.js

--- main.js
+++ main.js
@@ -5,5 +5,5 @@ const URLpar = window.location.search
 const urlParams = new URLSearchParams(URLpar)
 const param1 = urlParams.get('yt')
 if (param1 == "ternitanos") {
-  document.getElementById ("YT").innerHTML = "This is a demo of my websites! more comming soon! and "TerniTanos" has a litle surprize..."
+  document.getElementById ("YT").innerHTML = "This is a demo of my websites! more comming soon! and TerniTanos has a litle surprize..."
 }
