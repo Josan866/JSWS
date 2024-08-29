1c0783a027f2eb8038ce9a6ece03106b3ceb1bfd
Josan866 <joshua.millner@online.de>
2024-08-29 15:35:06 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 /*comment*/
 //comment roww
 
+const URLpar = window.location.search
+const urlParams = new URLSearchParams(URLpar)
+const param1 = urlParams.get('yt')
 if (param1 == ternitanos) {
   document.GetElementById ("terni").Innerhalb = " "
 }
