91bdb92f1cc296ecc232c6c57936e781472f1657
Josan866 <joshua.millner@online.de>
2024-08-30 23:47:00 +0200

    Update main.js

--- 1HS/main.js
+++ 1HS/main.js
@@ -1,3 +1,26 @@
-function hscodeentr(){
+//start
+const urlser = window.location.search
+const urlParams = new URLSearchParams(urlser)
+
+//functions
+function checkcode(){ //  ./entercode check passwords
+const param1 = urlParams.get('code')
+if (param1 == 997589) {
+    window.location = "./r3gisred"
+}
+else if (param1 == 2602200912082024) {
+    window.location = "./r3gisred?dog=7777666777777999"
+}
+else if (param1 == 728658) {
+    window.location = "../Test/Order"
+}
+else if (param1 == "bergungsalat") {
+    window.location = "../errors/1HS/RECOVERY/usb1"
+}
+else {window.location = "../ooor"}
+}
+
+
+function hscodeentr(){ //   ./entercode 
     window.location = "./entercode?scrt=728658";
 }
