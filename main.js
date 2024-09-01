8ea658ce2cae44795260ae6ec03a90fafcf60550
Josan866 <joshua.millner@online.de>
2024-09-01 21:23:55 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,8 @@
 //comment roww
 
 //setup
-const URLpar = window.location.search
-const urlParams = new URLSearchParams(URLpar)
+const "URLpar" = window.location.search
+const urlParams = new URLSearchParams("URLpar")
 
 //if serch params == ...
 let param1 = urlParams.get('WoInPr')
