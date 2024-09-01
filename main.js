13d796bee8fbd7e1d24cbe39d37f5dcf9f4fdf1e
Josan866 <joshua.millner@online.de>
2024-09-01 19:36:52 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 /*comment*/
 //comment roww
 
+//setup
 const URLpar = window.location.search
 const urlParams = new URLSearchParams(URLpar)
-const param1 = urlParams.get('yt')
+
+//if serch params == ...
+let param1 = urlParams.get('WoInPr')
+if (param1 == "workinntprogress"){
+  document.getElementById ("wip").innerHTML = ""
+}
+
+let param1 = urlParams.get('yt')
 if (param1 == "ternitanos") {
   document.getElementById ("YT").innerHTML = "This is a demo of my websites! more comming soon! and TerniTanos has a litle surprize..."
 }
