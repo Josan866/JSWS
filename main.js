566806c3c68719fff79ba7f94cb58e7554b15824
Josan866 <joshua.millner@online.de>
2024-09-01 21:02:49 +0200

    Update main.js

--- main.js
+++ main.js
@@ -8,10 +8,10 @@ const urlParams = new URLSearchParams(URLpar)
 //if serch params == ...
 let param1 = urlParams.get('WoInPr')
 if (param1 == "workinntprogress"){
-  document.getElementById ("wip").innerHTML = ""
+  document.getElementById ("wip").innerHTML = "";
 }
 
 let param1 = urlParams.get('yt')
 if (param1 == "ternitanos") {
-  document.getElementById ("YT").innerHTML = "This is a demo of my websites! more comming soon! and TerniTanos has a litle surprize..."
+  document.getElementById ("YT").innerHTML = "This is a demo of my websites! more comming soon! and TerniTanos has a litle surprize...";
 }
