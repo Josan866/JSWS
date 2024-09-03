df5b1c75c5e89110976480ddb1b2610ae5870416
Josan866 <joshua.millner@online.de>
2024-09-03 15:59:38 +0200

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ if (param1 == "workinntprogress"){
   document.getElementById ("wip").innerHTML = ""
 }
 
-let param1 = urlParams.get('yt')
-if (param1 == "ternitanos") {
+let param2 = urlParams.get('yt')
+if (param2 == "ternitanos") {
   document.getElementById ("YT").innerHTML = "This is a demo of my websites! more comming soon! and TerniTanos has a litle surprize..."
 }
