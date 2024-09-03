84a68fb97af248cc7dd5a360b86a0243a28bfa72
Josan866 <joshua.millner@online.de>
2024-09-03 18:28:18 +0200

    Update main.js

--- 1HS/main.js
+++ 1HS/main.js
@@ -22,21 +22,21 @@ else if (urlpar1 == "bergungsalat") {
 else if (urlpar1 == "talasgnugreb") {
     window.location = "../outoforder";
 }
-else {window.location = "../outoforder?atr=wc"};
+else {window.location = "../outoforder?atr=wc"}
 };
 
 
 function download(file){
     var link = document.getElementById("dcode2");
     link.setAttribute("download", file);
-    link.click()
+    link.click();
 };
 
 function terni(){  //   ./r3gisred check if terni
 urlpar1 = urlParams.get('dog');  // checking for search parameter
 if (urlpar1 == 7777666777777999) {
     document.getElementById("dog1").innerHTML = "sorry";
-    document.getElementById("dog2").innerHTML = "I´m sorry for your loss and i hope you get over it. You make awesome videos and i hope you continue. Kind reguards Josan866, marketing of J´s Markt Companies"
+    document.getElementById("dog2").innerHTML = "I´m sorry for your loss and i hope you get over it. You make awesome videos and i hope you continue. Kind reguards Josan866, marketing of J´s Markt Companies";
 }};
 
 
