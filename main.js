2c23f690606d7793edc36461399163d3f0cb849e
Josan866 <joshua.millner@online.de>
2024-09-03 16:07:24 +0200

    Update main.js

--- main.js
+++ main.js
@@ -6,12 +6,12 @@ const urlpar = window.location.search
 const urlParams = new URLSearchParams(urlpar)
 
 //if serch params == ...
-let param1 = urlParams.get('atribute')
-if (param1 == "workinntprogress"){
+var urlpar1 = urlParams.get('atribute')
+if (urlpar1 == "workinntprogress"){
   document.getElementById ("wip").innerHTML = ""
 }
 
-let param2 = urlParams.get('yt')
-if (param2 == "ternitanos") {
+var urlpar1 = urlParams.get('yt')
+if (urlpar1 == "ternitanos") {
   document.getElementById ("YT").innerHTML = "This is a demo of my websites! more comming soon! and TerniTanos has a litle surprize..."
 }
