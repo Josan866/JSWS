dd6d9f8cac2e95cc935b49d3128311489165be92
Josan866 <joshua.millner@online.de>
2024-09-03 16:28:28 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,14 +4,15 @@
 //setup
 const urlpar = window.location.search
 const urlParams = new URLSearchParams(urlpar)
+let urlpar1;
 
 //if serch params == ...
-var urlpar1 = urlParams.get('atribute')
+urlpar1 = urlParams.get('atribute');
 if (urlpar1 == "workinntprogress"){
   document.getElementById ("wip").innerHTML = ""
-}
+};
 
-var urlpar1 = urlParams.get('yt')
+urlpar1 = urlParams.get('yt');
 if (urlpar1 == "ternitanos") {
   document.getElementById ("YT").innerHTML = "This is a demo of my websites! more comming soon! and TerniTanos has a litle surprize..."
-}
+};
