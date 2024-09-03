f748313834d7c77e6f94b5689d6910dae4a4bd66
Josan866 <joshua.millner@online.de>
2024-09-03 16:16:37 +0200

    Update main.js

--- 1HS/main.js
+++ 1HS/main.js
@@ -4,17 +4,17 @@ const urlParams = new URLSearchParams(urlcd)
 
 //functions / check if urlParams == ...
 function checkcode(){ //  ./entercode check passwords
-let param1 = urlParams.get('code')  // checking for search parameter
-if (param1 == 997589) {
+var urlpar1 = urlParams.get('code')  // checking for search parameter
+if (urlpar1 == 997589) {
     window.location = "./r3gisred"
 }
-else if (param1 == 2602200912082024) {
+else if (urlpar1 == 2602200912082024) {
     window.location = "./r3gisred?dog=7777666777777999"
 }
-else if (param1 == 728658) {
+else if (urlpar1 == 728658) {
     window.location = "../Test/Order"
 }
-else if (param1 == "bergungsalat") {
+else if (urlpar1 == "bergungsalat") {
     window.location = "../errors/1HS/RECOVERY/usb1"
 }
 else {window.location = "../ooor"}
@@ -22,8 +22,8 @@ else {window.location = "../ooor"}
 
 
 function terni(){  //   ./r3gisred check if terni
-let param1 = urlParams.get('dog')  // checking for search parameter
-if (param1 == 7777666777777999) {
+var urlpar1 = urlParams.get('dog')  // checking for search parameter
+if (urlpar1 == 7777666777777999) {
     document.getElementById("dog1").innerHTML = "sorry";
     document.getElementById("dog2").innerHTML = "I´m sorry for your loss and i hope you get over it. You make awesome videos and i hope you continue. Kind reguards Josan866, marketing of J´s Markt Companies"
 }}
