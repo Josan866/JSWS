7e4a9dfdf0f8b021e67cd45961c0e6f37b37fb28
Josan866 <joshua.millner@online.de>
2024-09-03 16:29:06 +0200

    Update main.js

--- 1HS/main.js
+++ 1HS/main.js
@@ -5,29 +5,29 @@ let urlpar1;
 
 //functions / check if urlParams == ...
 function checkcode(){ //  ./entercode check passwords
-urlpar1 = urlParams.get('code')  // checking for search parameter
+urlpar1 = urlParams.get('code');  // checking for search parameter
 if (urlpar1 == 997589) {
     window.location = "./r3gisred"
-}
+};
 else if (urlpar1 == 2602200912082024) {
     window.location = "./r3gisred?dog=7777666777777999"
-}
+};
 else if (urlpar1 == 728658) {
     window.location = "../Test/Order"
-}
+};
 else if (urlpar1 == "bergungsalat") {
     window.location = "../errors/1HS/RECOVERY/usb1"
-}
+};
 else {window.location = "../ooor"}
-}
+};
 
 
 function terni(){  //   ./r3gisred check if terni
-urlpar1 = urlParams.get('dog')  // checking for search parameter
+urlpar1 = urlParams.get('dog');  // checking for search parameter
 if (urlpar1 == 7777666777777999) {
     document.getElementById("dog1").innerHTML = "sorry";
     document.getElementById("dog2").innerHTML = "I´m sorry for your loss and i hope you get over it. You make awesome videos and i hope you continue. Kind reguards Josan866, marketing of J´s Markt Companies"
-}}
+}};
 
 
 function hscodeentr(){ //   ./entercode 
