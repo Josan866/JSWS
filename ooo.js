d211fdf2996b37218bf44acc9c7b249da43852aa
Josan866 <joshua.millner@online.de>
2024-09-03 18:02:06 +0200

    Update ooo.js

--- ooo.js
+++ ooo.js
@@ -2,3 +2,22 @@
 
 // oooh ooop
 
+//setup
+const urlpar = window.location.search
+const urlParams = new URLSearchParams(urlpar)
+let urlpar1;
+let test;
+
+urlpar1 = urlParams.get('atr')
+if (urlpar1 == wc) {
+  document.getElementById("oooh").innerHTML = "Wrong Code";
+  document.getElementById("ooop").innerHTML = "Retry";
+  test = getElementById("ooop");
+  test.setAttribute("href", "./1HS/entercode");
+}
+else if (urlpar1 == dlf) {
+  document.getElementById("oooh").innerHTML = "Document downloaded";
+  document.getElementById("ooop").innerHTML = "To Homepage";
+  test = getElementById("ooop");
+  test.setAttribute("href", "./");
+}
