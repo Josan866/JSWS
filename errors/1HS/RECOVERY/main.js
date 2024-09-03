bd2edee14b6708335c914d2e5b8bbb993f7df8a3
Josan866 <joshua.millner@online.de>
2024-09-03 16:29:43 +0200

    Update main.js

--- errors/1HS/RECOVERY/main.js
+++ errors/1HS/RECOVERY/main.js
@@ -4,7 +4,7 @@ const urlParams = new URLSearchParams(urlpar)
 let urlpar1;
 
 //if serch params == ...
-urlpar1 = urlParams.get('yt')
+urlpar1 = urlParams.get('yt');
 if (urlpar1 == "ternitanos"){
   document.getElementById ("ytsound").innerHTML = "<audio autoplay loop><source src="./YTterni.wav" /></audio>"
 };
