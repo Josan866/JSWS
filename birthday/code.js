02d3abd83f7ca7b560dc6910efec1f31a596f8b0
Josan866 <joshua.millner@online.de>
2024-10-21 14:18:00 +0200

    Update code.js

--- birthday/code.js
+++ birthday/code.js
@@ -1,1 +1,22 @@
+//setup
+const urlcd = window.location.search
+const urlParams = new URLSearchParams(urlcd)
+let urlpar1;
 
+//functions / check if urlParams == ...
+function checkcode(){ //  ./entercode check passwords
+  urlpar1 = urlParams.get('code');  // checking for search parameter
+  if (urlpar1 == "birthdayboy") {
+      window.location = "./";
+  }
+  else if (urlpar1 == "halloween") {
+      window.location = "./invatation";
+  }
+  else {window.location = "../outoforder?atr=wc";
+};};
+
+function download(file){
+    var link = document.getElementById("dcode2");
+    link.setAttribute("href", file);
+    link.click();
+};
