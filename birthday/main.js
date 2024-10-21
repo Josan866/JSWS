95b75d2ca8557ea835e7ce1731be77e198ec9e9c
Josan866 <joshua.millner@online.de>
2024-10-21 14:45:24 +0200

    Update main.js

--- birthday/main.js
+++ birthday/main.js
@@ -6,6 +6,6 @@ let urlpar1;
 //if serch params == ...
 urlpar1 = urlParams.get('arg');
 if (urlpar1 == "true"){
-  document.getElementById ("argtrue").innerHTML = "Well, didn´t fool you... then we´ll proceed."
-  document.getElementById ("secret").innerHTML = ""
+  document.getElementById ("argtrue").innerHTML = 'Well, didn´t fool you... then we´ll proceed... <a href="./code?info=wow you found out">click here</a>'
+  document.getElementById ("secret").innerHTML = ''
 };
