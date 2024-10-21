2fc209e1574c4a24827b62da74ab6a93c81b75bc
Josan866 <joshua.millner@online.de>
2024-10-21 14:05:49 +0200

    Update main.js

--- birthday/main.js
+++ birthday/main.js
@@ -1,1 +1,10 @@
+//setup
+const urlpar = window.location.search
+const urlParams = new URLSearchParams(urlpar)
+let urlpar1;
 
+//if serch params == ...
+urlpar1 = urlParams.get('arg');
+if (urlpar1 == "true"){
+  document.getElementById ("argtrue").innerHTML = ""
+};
