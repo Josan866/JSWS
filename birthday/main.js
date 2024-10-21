0328fc9d86c8f4892a014ded582d7e1eafb096be
Josan866 <joshua.millner@online.de>
2024-10-21 14:08:19 +0200

    Update main.js

--- birthday/main.js
+++ birthday/main.js
@@ -6,5 +6,6 @@ let urlpar1;
 //if serch params == ...
 urlpar1 = urlParams.get('arg');
 if (urlpar1 == "true"){
-  document.getElementById ("argtrue").innerHTML = ""
+  document.getElementById ("argtrue").innerHTML = "Well, didn´t fool you... then we´ll proceed."
+  document.getElementById ("secret").innerHTML = ""
 };
