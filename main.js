adb0476ea13ad051763c4a60081f41e627e412a6
Josan866 <joshua.millner@online.de>
2024-12-11 20:11:07 +0100

    Update main.js

--- main.js
+++ main.js
@@ -7,12 +7,12 @@ const urlParams = new URLSearchParams(urlpar)
 let urlpar1;
 
 //if serch params == ...
-urlpar1 = urlParams.get('atribute');
+urlpar1 = urlParams.get('atr'); //atribute
 if (urlpar1 == "workinntprogress"){
   document.getElementById ("wip").innerHTML = ""
 };
 
-urlpar1 = urlParams.get('yt');
+urlpar1 = urlParams.get('yt'); //YouTube
 if (urlpar1 == "ternitanos") {
   document.getElementById ("YT").innerHTML = "This is a demo of my websites! more comming soon! and TerniTanos has a litle surprize..."
 };
