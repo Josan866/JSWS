8c6f61b06654f58c126421d1eca0e75a7e04ef51
Josan866 <joshua.millner@online.de>
2024-12-11 14:24:16 +0100

    Update main.js

--- 1HS/main.js
+++ 1HS/main.js
@@ -1,12 +1,12 @@
 //setup
 const urlcd = window.location.search
-const urlParams = new URLSearchParams(urlcd)
-let urlpar1;
+const urlqm = new URLSearchParams(urlcd)
+let urlq;
 
 //urlpar1 = urlParams.get('code'); checking for search parameter
 function secretcode(){
-urlpar1 = urlParams.get('secret'); 
-if (urlpar1 == "true") {
+urlq = urlqm.get('secret'); 
+if (urlq == "true") {
     document.getElementById("ecd").innerHTML = '
 	    <form action="./3nte4c0d3s2" method="GET>"
     		<label for="k0b3">Enter Code:</label>
@@ -22,8 +22,8 @@ function download(file){
 };
 
 function terni(){  //   ./r3gisred check if terni
-urlpar1 = urlParams.get('dog');  // checking for search parameter
-if (urlpar1 == 7777666777777999) {
+urlq = urlqm.get('dog');  // checking for search parameter
+if (urlq == 7777666777777999) {
     document.getElementById("dog1").innerHTML = "sorry";
     document.getElementById("dog2").innerHTML = "I´m sorry for your loss and i hope you get over it. You make awesome videos and i hope you continue. Kind reguards Josan866, marketing of J´s Markt Companies";
 }};
