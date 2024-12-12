28c1b952de4213963524d193916fcc537c7b05b1
Josan866 <joshua.millner@online.de>
2024-12-12 11:17:37 +0100

    Update code2.js

--- 1HS/code2.js
+++ 1HS/code2.js
@@ -10,7 +10,16 @@ if (urlq == 190) {
     window.location = "./r3gisred";
 }
 else if (urlq == "Code"){
-    window.location = "..."
+    window.location = "./entercode?secret=true&wc=1"
+}
+else if (urlq == "incorrect"){
+    window.location = "./entercode?secret=true&wc=2"
+}
+    else if (urlq == "again"){
+    window.location = "./entercode?secret=true&wc=3"
+}
+        else if (urlq == "again later"){
+    window.location = "./bruh"
 }
 else {window.location = "../outoforder?origin=secret&atr=wc";}
 };
