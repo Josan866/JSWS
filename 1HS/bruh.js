521bf1ffa17a8b3ddba2b27c2296de6705fef651
Josan866 <joshua.millner@online.de>
2024-12-12 14:23:29 +0100

    Update bruh.js

--- 1HS/bruh.js
+++ 1HS/bruh.js
@@ -1,5 +1,15 @@
-//js
+//setup
+const urlcd = window.location.search
+const urlqm = new URLSearchParams(urlcd)
+let urlq;
 
+//functions
+function viract(){
+    urlq = urlqm.get('viract')
+    if (urlq == "viract") {
+        download(./DATA/Autoreboot-Death.batx);
+    };
+};
 
 function download(file){
     var link = document.getElementById("dcode2");
