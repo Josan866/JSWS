c9a19c828432e4424b8158f4e67b84dcb80fb524
Josan866 <joshua.millner@online.de>
2024-12-12 11:08:32 +0100

    Update code2.js

--- 1HS/code2.js
+++ 1HS/code2.js
@@ -1,13 +1,16 @@
 //setup
 const urlcd = window.location.search
-const urlParams = new URLSearchParams(urlcd)
-let urlpar1;
+const urlqm = new URLSearchParams(urlcd)
+let urlq;
 
 //functions / check if urlParams == ...
 function checkcode(){ //  ./entercode check passwords
-urlpar1 = urlParams.get('code');  // checking for search parameter
-if (urlpar1 == 190) {
+urlq = urlqm.get('code');  // checking for search parameter
+if (urlq == 190) {
     window.location = "./r3gisred";
+}
+else if (urlq == "Code"){
+    window.location = "..."
 }
 else {window.location = "../outoforder?origin=secret&atr=wc";}
 };
