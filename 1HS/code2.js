2b710a9eae3bc4454b04e515df13d06c9d56ec64
Josan866 <joshua.millner@online.de>
2025-05-08 14:48:33 +0200

    Update code2.js

--- 1HS/code2.js
+++ 1HS/code2.js
@@ -6,19 +6,21 @@ let urlq;
 //functions / check if urlParams == ...
 function checkcode(){ //  ./entercode check passwords
 urlq = urlqm.get('code');  // checking for search parameter
+wccheck = urlqm.get('wc')
+    
 if (urlq == 190) {
     window.location = "./r3gisred";
 }
 else if (urlq == "Code"){
     window.location = "./entercode?secret=true&wc=1"
 }
-else if (urlq == "incorrect"){
+else if (urlq == "incorrect" && wccheck == 1){
     window.location = "./entercode?secret=true&wc=2"
 }
-    else if (urlq == "again"){
+    else if (urlq == "again" && wccheck == 2){
     window.location = "./entercode?secret=true&wc=3"
 }
-        else if (urlq == "again later"){
+        else if (urlq == "again later"  && wccheck == 3){
     window.location = "./bruh"
 }
 else {window.location = "../outoforder?origin=secret&atr=wc";}
