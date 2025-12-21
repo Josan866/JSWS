6605bc286334bb8e81e41aa5e9b0ce317d6e15a6
Josan866 <joshua.millner@online.de>
2025-12-21 22:56:46 +0100

    Create main.js

---
+++ outoforder/main.js
@@ -0,0 +1,57 @@
+// out of order and Wrong Code Java Script from J´s Markt
+// Orginal page: https://jsws.cinterop.de/outoforder
+
+//setup
+const urlpar = window.location.search
+const urlParams = new URLSearchParams(urlpar)
+let urlpar1;
+
+urlpar1 = urlParams.get('atr')
+if (urlpar1 == "wc") {
+  document.getElementById("oooh").innerHTML = "Wrong Code";
+  document.getElementById("ooop").innerHTML = "Retry";
+  document.getElementById("ooot").innerHTML = "Invalide Code";
+  urlpar1 = urlParams.get('origin')
+  if (urlpar1 == "birth") { //birthday edition
+    let test = document.getElementById("ooop");
+    test.setAttribute("href", "./birthday/code");
+  }
+  else if (urlpar1 == "secret") { //1HS secret codes
+    let test = document.getElementById("ooop");
+    test.setAttribute("href", "./1HS/entercode?secret=true");
+  }
+  else if (urlpar1 == "horscp") { //1HS
+    let test = document.getElementById("ooop");
+    test.setAttribute("href", "./1HS/entercode?scrt=728658");
+  }
+  else if (urlpar1 == "xmas") { //XMAS
+    let test = document.getElementById("ooop");
+    test.setAttribute("href", "./xmas/");
+  }
+  else {
+    window.location = "./outoforder";
+  };
+}
+else if (urlpar1 == "dlf") { //down-load finished
+  document.getElementById("oooh").innerHTML = "Document downloaded";
+  document.getElementById("ooop").innerHTML = "Back to Homepage";
+  document.getElementById("ooot").innerHTML = "Downloaded";
+  let test = document.getElementById("ooop");
+  test.setAttribute("href", "./");
+}
+else if (urlpar1 == "dcs") { //download celebration script
+  document.getElementById("oooh").innerHTML = "End Monolog downloaded";
+  document.getElementById("ooop").innerHTML = "Try HorrorScape";
+  document.getElementById("ooot").innerHTML = "Downloaded";
+  let test = document.getElementById("ooop");
+  test.setAttribute("href", "./1HS/");
+}
+else if (urlpar1 == "dlfa") { //down-load finished audio
+  document.getElementById("oooh").innerHTML = "Document downloaded";
+  document.getElementById("oooc").innerHTML = '<h2 class="oooh"><a href="./CAUTION/audio">Caution: Loud audio</a></h2>';
+  document.getElementById("ooop").innerHTML = "Back to Homepage";
+  document.getElementById("ooot").innerHTML = "Downloaded";
+  let test = document.getElementById("ooop");
+  test.setAttribute("href", "./");
+} 
+else {};
