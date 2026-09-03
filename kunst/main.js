3ac0f83e4d3d2496474c8597cab783b445f91d6f
Josan866 <joshua.millner@online.de>
2026-09-03 15:02:12 +0200

    Add hacking simulation script in main.js
    This script simulates a hacking sequence with loading messages and error notifications. It updates the HTML content dynamically based on a timer.

---
+++ kunst/main.js
@@ -0,0 +1,70 @@
+const hack_p = "hack_bg"; // hack_bg html ID
+const time = 20;           // in Sekunden
+const time_to_next = 100; // in milliSekunden
+
+setTimeout(hack, time * 1000);
+
+function hack(){
+    document.getElementById('body').innerHTML = "<div id=\"hack_bg\" style=\"top:0px\">Initializing</div><div id=\"overlay\"></div>";
+
+    let dot = setInterval(function(){
+        document.getElementById('hack_bg').innerHTML = document.getElementById('hack_bg').innerHTML + ".";
+    }, 50);
+
+    let repetitions = -30;
+    setInterval(function(){
+        switch (repetitions) {
+            case 0:
+                clearInterval(dot);
+                break;
+            case 1:
+                code("Loading Code...");
+                break;
+            case 2:
+                code_err("Syntax Error in line 12");
+                break;
+            case 5:
+                code("restarting Code");
+                break;
+            case 6:
+                code("Loading Files" + "<br>");
+                break;
+            case 20:
+                code("READ_ME.md");
+                code("Don't investigate.txt");
+                code("[REDACTED]");
+                code("[REDACTED]");
+                code("Diary.txt");
+                code("[REDACTED]");
+                code("[REDACTED]");
+                code("Lamp.js");
+                code("Help.png");
+                code("[REDACTED]" + "<br>");
+                break;
+            case 21:
+                code("Running Script");
+                break;
+            case 30:
+                code("Don't Investiate");
+                break;
+        }
+        if(repetitions > 50 && repetitions < 999999999){
+            scroll(5);
+        }
+        repetitions += 1;
+    }, time_to_next);
+}
+
+function code(text){
+    document.getElementById(hack_p).innerHTML = document.getElementById(hack_p).innerHTML + "<br><span>05.02.2026&nbsp;&nbsp;&nbsp;14:52&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + text + "</span>";
+}
+
+function code_err(text){
+    document.getElementById(hack_p).innerHTML = document.getElementById(hack_p).innerHTML + "<br><span style=\"color:var(--hack-text-error)\">05.02.2026&nbsp;&nbsp;&nbsp;14:52&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + text + "</span>";
+}
+
+function scroll(px){ // Eine Zeile = 20px
+    let prevpx = parseInt(document.getElementById(hack_p).getAttribute("style").split(":")[1])
+    let math = parseInt(prevpx - px);
+    document.getElementById(hack_p).setAttribute("style","top:" + String(math) + "px")
+}
